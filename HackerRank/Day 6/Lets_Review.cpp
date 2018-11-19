74b2ecbada4114e2d0fd6b44c4f9111ff9570525
Sujal <sujalahrodia@gmail.com>
2018-11-18 19:44:11 -0500

    Create Lets_Review.cpp

---
+++ HackerRank/Day 6/Lets_Review.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t=0;
+    cin>>t;
+
+    while(t>0)
+    {
+        string str,stre,stro;
+        cin>>str;
+        int j =0, k=0;
+
+        for(int i=0; i<str.length(); i++)
+        {
+            if(i%2 == 0)
+                cout<<str[i];
+        }
+        cout<<" ";
+        for(int i=0; i<str.length(); i++)
+        {
+            if(i%2 != 0)
+                cout<<str[i];
+        }
+        cout<<endl;
+        t--;
+    }
+    return 0;
+}
+
