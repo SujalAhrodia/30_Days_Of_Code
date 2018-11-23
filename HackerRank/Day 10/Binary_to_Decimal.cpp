393df83911619c43947acaeea1eb47e5c979af13
Sujal <sujalahrodia@gmail.com>
2018-11-22 20:28:33 -0500

    Update Binary_to_Decimal.cpp

--- HackerRank/Day 10/Binary_to_Decimal.cpp
+++ HackerRank/Day 10/Binary_to_Decimal.cpp
@@ -7,7 +7,8 @@ int main()
     int n;
     cin >> n;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
+    
+    //The number of iterations required to reach 0 is actually length of the longest consecutive sequence of 1s
     int count=0;
     while(n!=0)
     {
